0e50713595b3d975c1b8b5c83fa0f85c6c18dc2b
Ivan Gamez <thecubeg@gmail.com>
2025-09-05 15:16:58 -0500

    feat vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 // https://vitejs.dev/guide/build.html#multi-page-app
 
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        products: resolve(__dirname, 'src/pages/products/products.html'),
+        main: resolve(__dirname, "index.html"),
+        products: resolve(__dirname, "src/pages/products/products.html"),
+        register: resolve(__dirname, "src/pages/register/register.html"),
         /*aboutUs: resolve(__dirname, 'src/pages/about-us/about-us.html'),
         contact: resolve(__dirname, 'src/pages/contact/contact.html'),
         register: resolve(__dirname, 'src/pages/register/register.html'),*/
       },
     },
   },
-})
+});
